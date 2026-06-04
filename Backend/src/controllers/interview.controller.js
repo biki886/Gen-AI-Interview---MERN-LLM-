62af100b42f7ca29067034dadadbaed649d36c39
Biki Haldar <147740771+biki886@users.noreply.github.com>
2026-06-04 17:52:57 +0530

    Update interview.controller.js

--- Backend/src/controllers/interview.controller.js
+++ Backend/src/controllers/interview.controller.js
@@ -76,6 +76,8 @@ async function generateResumePdfController(req, res) {
     const { interviewReportId } = req.params
 
     const interviewReport = await interviewReportModel.findById(interviewReportId)
+    console.log("PDF API HIT");
+console.log("REPORT:", interviewReport);
 
     if (!interviewReport) {
         return res.status(404).json({
